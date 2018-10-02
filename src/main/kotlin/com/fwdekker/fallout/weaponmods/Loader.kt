1082ab68af2dbb2d5df7e48c65097a9fcab1d5af
Felix Dekker <felix@fwdekker.com>
2018-10-02 23:06:59 +0200

    Ignore case during ESM/weapon lookup

--- src/main/kotlin/com/fwdekker/fallout/weaponmods/Loader.kt
+++ src/main/kotlin/com/fwdekker/fallout/weaponmods/Loader.kt
@@ -36,12 +36,15 @@ data class WeaponMod(
         database: GameDatabase
     ) :
         this(
-            esm = ESMs[looseMod.file.toLowerCase()]!!, // TODO handle error
+            esm = ESM.get(looseMod.file)!!, // TODO handle error
             formIDTemplate = looseMod.formID.toString(16).let {
                 if (it.length > 6) "{{DLC ID|${it.take(6)}}}"
                 else "{{ID|$it}}"
             },
-            weapon = Weapons[objectModifier.weaponName.toLowerCase()] ?: Weapon("", "", "", ""), // TODO handle nulls!
+            weapon = Weapon.get(objectModifier.weaponName.toLowerCase()) ?: Weapon("",
+                "",
+                "",
+                ""), // TODO handle nulls!
             effects = objectModifier.description,
             components = craftableObject.components
                 .map { Pair(database.components.single { c -> c.editorID == it.component }, it.count) },
@@ -80,15 +83,20 @@ data class ESM(
     val modCategory: String? = null
 ) {
     fun getWikiLink() =
-        if (name == link) "''[[$name]]''"
+        if (name.capitalize() == link) "''[[$name]]''"
         else "''[[$link|$name]]''"
-}
 
-// TODO move JSON to suitable location (same for Weapons)
-// TODO handle errors (same for Weapons)
-val ESMs = Klaxon().parseArray<ESM>(File("esms.json").inputStream())!!
-    .map { Pair(it.fileName.toLowerCase(), it) }
-    .toMap()
+
+    companion object {
+        // TODO move JSON to suitable location (same for Weapons)
+        // TODO handle errors (same for Weapons)
+        private val esms = Klaxon().parseArray<ESM>(File("esms.json").inputStream())!!
+            .map { Pair(it.fileName.toLowerCase(), it) }
+            .toMap()
+
+        fun get(fileName: String) = esms[fileName.toLowerCase()]
+    }
+}
 
 data class Weapon(
     val file: String,
@@ -97,13 +105,18 @@ data class Weapon(
     val page: String
 ) {
     fun getWikiLink() =
-        if (name == page) "[[${name.capitalize()}]]"
+        if (name.capitalize() == page) "[[${name.capitalize()}]]"
         else "[[$page|${name.capitalize()}]]"
-}
 
-val Weapons = Klaxon().parseArray<Weapon>(File("weapons.json").inputStream())!!
-    .map { Pair(it.keyword.toLowerCase(), it) }
-    .toMap()
+
+    companion object {
+        private val weapons = Klaxon().parseArray<Weapon>(File("weapons.json").inputStream())!!
+            .map { Pair(it.keyword.toLowerCase(), it) }
+            .toMap()
+
+        fun get(keyword: String) = weapons[keyword.toLowerCase()]
+    }
+}
 
 fun modelToImage(model: String): String =
     when (model.toLowerCase()) {
@@ -131,7 +144,7 @@ fun main(args: Array<String>) {
 }
 
 private fun launch(database: GameDatabase, modName: String) {
-    val looseMods = database.looseMods.asSequence().filter { it.name.toLowerCase().contains(modName) }.toList()
+    val looseMods = database.looseMods.asSequence().filter { it.name.toLowerCase().contains(modName.toLowerCase()) }.toList()
     val weaponMods = looseMods.asSequence()
         .mapNotNull { WeaponMod.create(it, database) }
         .sortedBy { it.weapon.name }
@@ -145,18 +158,18 @@ private fun launch(database: GameDatabase, modName: String) {
         weaponMods.groupingBy { it.image }.eachCount().entries.maxBy { it.value }?.key ?: "" // TODO log empty image
     val games = weaponMods.asSequence().mapNotNull { it.esm }.distinct().toList().sortedBy { it.name }
     val appearanceString =
-        if (games.size == 1 && games[0] == ESMs["fallout4.esm"])
+        if (games.size == 1 && games[0] == ESM.get("Fallout4.esm"))
             games[0].getWikiLink()
-        else if (games.size == 2 && games.contains(ESMs["fallout4.esm"]))
-            "${ESMs["fallout4.esm"]!!.getWikiLink()} and its [[Fallout 4 add-ons|add-on]] ${games.asSequence()
-                .filterNot { it == ESMs["fallout4.esm"] }
+        else if (games.size == 2 && games.contains(ESM.get("Fallout4.esm")))
+            "${ESM.get("Fallout4.esm")!!.getWikiLink()} and its [[Fallout 4 add-ons|add-on]] ${games.asSequence()
+                .filterNot { it == ESM.get("Fallout4.esm") }
                 .first()
                 .getWikiLink()
             }"
         else
-            "${ESMs["fallout4.esm"]!!.getWikiLink()} and its [[Fallout 4 add-ons|add-ons]] ${games.dropLast(1)
+            "${ESM.get("Fallout4.esm")!!.getWikiLink()} and its [[Fallout 4 add-ons|add-ons]] ${games.dropLast(1)
                 .asSequence()
-                .filterNot { it == ESMs["fallout4.esm"] }
+                .filterNot { it == ESM.get("Fallout4.esm") }
                 .map { it.getWikiLink() }
                 .joinToString(", ")
             } and ${games.last().getWikiLink()}"
